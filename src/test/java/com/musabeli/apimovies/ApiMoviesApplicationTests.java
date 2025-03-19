2f7ace54ac142225b0602b05b9b4a16096de457b
Albania Musabeli <musabelialbania@gmail.com>
2025-03-19 18:53:40 -0300

    chore(initial-commit): Crear proyecto springboot API movies

---
+++ src/test/java/com/musabeli/apimovies/ApiMoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.musabeli.apimovies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiMoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
